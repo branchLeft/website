87b8a68e5d22cd5fc6bf2cb32f4cf778550036c2
Robert Murray <rob@branchleft.co.uk>
2026-07-26 14:47:42 +0100

    remove boilerplate; add logo; add placeholder content; define fonts; define initial colour theme

--- app/root.tsx
+++ app/root.tsx
@@ -10,18 +10,10 @@ import {
 
 import type { Route } from './+types/root';
 import './app.css';
+import './app.scss';
 
 export const links: Route.LinksFunction = () => [
-  { rel: 'preconnect', href: 'https://fonts.googleapis.com' },
-  {
-    rel: 'preconnect',
-    href: 'https://fonts.gstatic.com',
-    crossOrigin: 'anonymous',
-  },
-  {
-    rel: 'stylesheet',
-    href: 'https://fonts.googleapis.com/css2?family=Inter:ital,opsz,wght@0,14..32,100..900;1,14..32,100..900&display=swap',
-  },
+  { rel: 'icon', type: 'image/svg+xml', href: '/logo.svg' },
 ];
 
 export function Layout({ children }: { readonly children: React.JSX.Element }): React.JSX.Element {
